141f84f7f61d129bb88e93f4615671a5e143dd23
Mateus-Barreto <43544329+Mateus-Barreto@users.noreply.github.com>
2018-10-09 22:40:29 -0300

    Update main.m

--- Funcional MatLab/Recebendo dados URL/main.m
+++ Funcional MatLab/Recebendo dados URL/main.m
@@ -1,10 +1,10 @@
 api = 'http://vanderlei.ga/cervical/';
 url = [api 'test.json'];
 S = webread(url);
-year = [S.year];
-data = [S.data];
-plot(year,data)
-xlabel('Year');
-ylabel('Temperature (Celsius)');
-title('USA Average Temperatures')
+X = [S.X];
+Y = [S.Y];
+plot(X,Y);
+xlabel('Tempo');
+ylabel('Graus');
+title('Inclinação do pescoço do usuário com relação ao tempo.')
 axis tight
